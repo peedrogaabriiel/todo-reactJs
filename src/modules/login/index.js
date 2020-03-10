9aa2f94252a1af971f0dfdbe26e1185fbde0fbb8
Pedro Gabriel Salazar Daltoe <pedro.daltoe@cwi.com.br>
2020-03-10 16:59:26 -0300

    commit ajustes flex

--- src/modules/login/index.js
+++ src/modules/login/index.js
@@ -5,58 +5,77 @@ const Login = () => {
   const [password, setPassword] = useState("");
 
   const submit = () => {
-    if (email === "pedro" && password === "pedro") {
+    if (email === "pedro@email.com" && password === "pedro") {
       alert("ok");
     } else {
       alert("tente novamente");
     }
   };
 
-  const onChangeEmail = () => {
-    setEmail({ email: email });
+  const onChangeEmail = event => {
+    setEmail(event.target.value);
   };
+
+  const onChangePassword = event => {
+    setPassword(event.target.value);
+  };
+
   return (
     <div
       className="card bg-dark text-white"
       style={{
         background: "gray",
-        height: 1000
+        flex: 1,
+        height: "100%",
+        justifyContent: "center"
       }}
     >
-      <form
-        onSubmit={submit}
-        className="container"
+      <div
         style={{
-          width: 500
+          background: "red",
+          width: 600,
+          height: 300,
+          alignSelf: "center",
+          alignContent: "center",
+          borderRadius: 14
         }}
       >
-        <h3>Login</h3>
+        <form
+          onSubmit={submit}
+          className="container"
+          style={{
+            width: 500
+          }}
+        >
+          <h3>Login</h3>
 
-        <div className="form-group">
-          <label>E-mail</label>
-          <input
-            onChange={onChangeEmail}
-            value={email}
-            type="email"
-            className="form-control"
-            placeholder="Insira seu e-mail"
-          />
-        </div>
+          <div className="form-group">
+            <label>E-mail</label>
+            <input
+              onChange={onChangeEmail}
+              value={email}
+              type="email"
+              className="form-control"
+              placeholder="Insira seu e-mail"
+            />
+          </div>
 
-        <div className="form-group">
-          <label>Senha</label>
-          <input
-            value={password}
-            type="password"
-            className="form-control"
-            placeholder="Insira sua senha"
-          />
-        </div>
+          <div className="form-group">
+            <label>Senha</label>
+            <input
+              onChange={onChangePassword}
+              value={password}
+              type="password"
+              className="form-control"
+              placeholder="Insira sua senha"
+            />
+          </div>
 
-        <button type="submit" className="btn btn-primary btn-block">
-          Entrar
-        </button>
-      </form>
+          <button type="submit" className="btn btn-primary btn-block">
+            Entrar
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
