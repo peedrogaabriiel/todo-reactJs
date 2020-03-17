b58fd7d4413be956ead4d0b3ba98707abc758c73
Pedro Gabriel Salazar Daltoe <pedro.daltoe@cwi.com.br>
2020-03-17 14:14:21 -0300

    criação do header e ação para logout removendo item de autenticação do localStorage

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,8 +1,25 @@
 import React from "react";
 import "./styles.css";
+import NavigationService from "../../services/navigation-service";
+import routesNames from "../../router/routes-names";
 
 const Header = () => {
-  return <header id="main-header">Dragons School</header>;
+  const logout = () => {
+    localStorage.removeItem("authenticated");
+    NavigationService.push(routesNames.login);
+    window.location.reload();
+    NavigationService.push(routesNames.login);
+  };
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <span className="span">Dragons School</span>
+      <div className="div-inline my-2 my-lg-0">
+        <button className="button" onClick={logout}>
+          Sair
+        </button>
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
